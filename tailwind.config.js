8a1a06128fcc1c2508e6e7c72d9a54d6c8ef8f6c
latched <spigotemsi@gmail.com>
2024-11-24 10:01:29 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#7e22ce",
+        sec: "#eab308",
+        teks: "#78716c"
+      }
+    },
+  },
+  plugins: [],
+}
